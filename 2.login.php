cec2dba1b0288dd471e50cfc63e9ddbdd3eac6ee
wenpei3 <wensusan0222@gmail.com>
2025-04-11 23:16:06 +0800

    Create 2.login.php

---
+++ 2.login.php
@@ -0,0 +1,11 @@
+<?php 
+    echo $_POST["id"];
+    echo "<br>";
+    echo $_POST["pwd"];
+?>
+
+<?php 
+    echo $_POST["id"];  // 輸出使用者輸入的帳號（從表單的 id 欄位接收）
+    echo "<br>";  // 在帳號和密碼之間插入換行
+    echo $_POST["pwd"];  // 輸出使用者輸入的密碼（從表單的 pwd 欄位接收）
+?>
