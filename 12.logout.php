6972b794459f6a5cb202ad77b8b89f4219e5edc5
wenpei3 <wensusan0222@gmail.com>
2025-04-12 00:24:03 +0800

    Create 12.logout.php

---
+++ 12.logout.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    unset($_SESSION["id"]);
+    echo "登出成功....";
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+
+?>
+
+<?php
+    // 啟動會話，這樣可以存取 session 變數
+    session_start();
+
+    // 刪除 session 中的 "id" 變數，實現登出
+    unset($_SESSION["id"]);
+
+    // 顯示登出成功訊息
+    echo "登出成功....";
+
+    // 使用 meta 標籤在 3 秒後自動重定向到登入頁面
+    echo "<meta http-equiv='REFRESH' content='3; url=2.login.html'>";
+?>
