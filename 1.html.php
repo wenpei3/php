6a78dfe01867b1c1f8f486339f49e25474244265
wenpei3 <wensusan0222@gmail.com>
2025-04-11 23:12:17 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>html+php</title> <!-- 網頁標題 -->
+    </head>      
+    <body>
+        <?php echo "Hello world!"; ?>  <!-- 使用 PHP 輸出 Hello world! -->
+        
+        <h1>html</h1>  <!-- HTML 大標題 -->
+        
+        <?php echo "Hello world!"; ?>  <!-- 再次輸出 Hello world! -->
+        
+        <h1>html</h1>  <!-- 再次顯示 HTML 大標題 -->
+        
+        <?php echo "Hello world!"; ?>  <!-- 第三次輸出 Hello world! -->
+    </body>
+</html>
