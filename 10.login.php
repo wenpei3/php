0e0ade41973776cc02aabc511526ddde3a1c9e38
wenpei3 <wensusan0222@gmail.com>
2025-04-12 21:46:09 +0800

    Update 10.login.php

--- 10.login.php
+++ 10.login.php
@@ -23,6 +23,8 @@
   }
 ?>
 
+
+註解
 <?php
    # mysqli_connect() 建立資料庫連結
    $conn = mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
@@ -30,33 +32,33 @@
    # mysqli_query() 從資料庫查詢資料
    $result = mysqli_query($conn, "SELECT * FROM user");
 
-   # 設定登入狀態為預設 FALSE
+   //設定登入狀態為預設 FALSE
    $login = FALSE;
 
-   # 使用 while 循環來遍歷資料庫查詢結果
+   //使用 while 循環來遍歷資料庫查詢結果
    while ($row = mysqli_fetch_array($result)) {
-     # 檢查帳號和密碼是否匹配
+     //檢查帳號和密碼是否匹配
      if (($_POST["id"] == $row["id"]) && ($_POST["pwd"] == $row["pwd"])) {
        $login = TRUE;
      }
    }
 
-   # 檢查是否登入成功
+   //檢查是否登入成功
    if ($login == TRUE) {
-     # 登入成功，開始會話並設置 session 變數
+     //登入成功，開始會話並設置 session 變數
      session_start();
      $_SESSION["id"] = $_POST["id"];  // 將帳號儲存到 session 中
 
-     # 顯示登入成功訊息
+     //顯示登入成功訊息
      echo "登入成功";
      
-     # 使用 meta 標籤進行頁面重定向，3秒後跳轉到 11.bulletin.php
+     //使用 meta 標籤進行頁面重定向，3秒後跳轉到 11.bulletin.php
      echo "<meta http-equiv='REFRESH' content='3; url=11.bulletin.php'>";
    } else {
-     # 登入失敗，顯示錯誤訊息
+     //登入失敗，顯示錯誤訊息
      echo "帳號/密碼 錯誤";
 
-     # 使用 meta 標籤進行頁面重定向，3秒後跳轉回 2.login.html
+     //使用 meta 標籤進行頁面重定向，3秒後跳轉回 2.login.html
      echo "<meta http-equiv='REFRESH' content='3; url=2.login.html'>";
    }
 ?>
