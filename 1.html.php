b627effd40efd5557dcb6c027f338f796ca45d9c
wenpei3 <wensusan0222@gmail.com>
2025-04-12 21:48:13 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,16 +1,29 @@
 <html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
+
+
+註解
+<html>
     <head>
-        <title>html+php</title> <!-- 網頁標題 -->
+        <title>html+php</title> //網頁標題 
     </head>      
     <body>
-        <?php echo "Hello world!"; ?>  <!-- 使用 PHP 輸出 Hello world! -->
+        <?php echo "Hello world!"; ?>  //使用 PHP 輸出 Hello world! 
         
-        <h1>html</h1>  <!-- HTML 大標題 -->
+        <h1>html</h1>  //HTML 大標題 
         
-        <?php echo "Hello world!"; ?>  <!-- 再次輸出 Hello world! -->
+        <?php echo "Hello world!"; ?>  // 再次輸出 Hello world!
         
-        <h1>html</h1>  <!-- 再次顯示 HTML 大標題 -->
+        <h1>html</h1>  // 再次顯示 HTML 大標題 
         
-        <?php echo "Hello world!"; ?>  <!-- 第三次輸出 Hello world! -->
+        <?php echo "Hello world!"; ?>  // 第三次輸出 Hello world! 
     </body>
 </html>
